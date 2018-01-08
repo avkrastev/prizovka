7537cfd91da0d0bf083ca64d0981cf9e4dad665e
Alexander Krastev <avkrastev@gmail.com>
2018-01-08 22:50:24 +0200

    changed: config.ini; added: users seeder

--- app/migrations/1.0.1/users.php
+++ app/migrations/1.0.1/users.php
@@ -47,8 +47,96 @@ class UsersMigration_101 extends Migration
                 1,
                 'Александър',
                 'Кръстев',
-                'avkrastev1@gmail.com',
+                'avkrastev@gmail.com',
                 'c0bd96dc7ea4ec56741a4e07f6ce98012814d853',
+                1
+            ],
+            [
+                'org',
+                'type',
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+                'active'
+            ]
+        );
+
+        self::$_connection->insert(
+            'users',
+            [
+                1,
+                2,
+                'Иван',
+                'Стоянов',
+                'ivan@example.com',
+                'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4',
+                1
+            ],
+            [
+                'org',
+                'type',
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+                'active'
+            ]
+        );
+
+        self::$_connection->insert(
+            'users',
+            [
+                1,
+                3,
+                'Димитър',
+                'Христов',
+                'dimitar@example.com',
+                'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4',
+                1
+            ],
+            [
+                'org',
+                'type',
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+                'active'
+            ]
+        );
+
+        self::$_connection->insert(
+            'users',
+            [
+                1,
+                4,
+                'Йордан',
+                'Милушев',
+                'jordan@example.com',
+                'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4',
+                1
+            ],
+            [
+                'org',
+                'type',
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+                'active'
+            ]
+        );
+
+        self::$_connection->insert(
+            'users',
+            [
+                1,
+                5,
+                'Стоян',
+                'Колев',
+                'stoyan@example.com',
+                'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4',
                 1
             ],
             [
