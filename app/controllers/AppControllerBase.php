6a112ae29f0fe27b64a342565e01998ee46d9d47
Alexander Krastev <avkrastev@gmail.com>
2018-03-18 12:24:33 +0200

    set default template for app

--- app/controllers/AppControllerBase.php
+++ app/controllers/AppControllerBase.php
@@ -7,6 +7,7 @@ class AppControllerBase extends CommonController
 
     protected function initialize()
     {
+        $this->view->setTemplateAfter('app');
         $this->view->setMainView('app');
 
         parent::initialize();
